569890a0ee4732ef87ad24dada0248c54ec9ee88
Antony Lemos <lemosantony@gmail.com>
2020-10-12 10:44:08 -0300

    chore: refactoring delete method

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Antony',
-    email: 'antony@mail.com',
-    phone: '77777777',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Matheus',
-    email: 'matheus@mail.com',
-    phone: '77777777',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -61,11 +42,10 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id !== id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+
+    return deleteOp;
   }
 }
 
